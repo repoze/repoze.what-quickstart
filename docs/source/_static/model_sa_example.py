56c61072d869c0d6f18c0b1ad173c157e87a356c
Gustavo Narea <me@gustavonarea.net>
2009-02-10 18:32:57 +0000

    Fixed more bugs in sample models

--- docs/source/_static/model_sa_example.py
+++ docs/source/_static/model_sa_example.py
@@ -6,8 +6,8 @@ but it's absolutely independent of TurboGears.
 
 """
 
-import md5
-import sha
+import os
+from hashlib import sha1
 from datetime import datetime
 
 from sqlalchemy import Table, ForeignKey, Column
@@ -63,49 +63,49 @@ class User(DeclarativeBase):
     _password = Column('password', Unicode(80))
 
     def _set_password(self, password):
-        """encrypts password on the fly."""
-        self._password = self.__encrypt_password(password)
-
-    def _get_password(self):
-        """returns password"""
-        return self._password
-
-    password = synonym('password', descriptor=property(_get_password,
-                                                       _set_password))
-
-    def __encrypt_password(self, password):
-        """Hash the given password with SHA1.
-        
-        Edit this method to implement your own algorithm.
-        
-        """
+        """Hash password on the fly."""
         hashed_password = password
 
         if isinstance(password, unicode):
             password_8bit = password.encode('UTF-8')
-
         else:
             password_8bit = password
-
-        hashed_password = sha.new(password_8bit).hexdigest()
+        
+        salt = sha1()
+        salt.update(os.urandom(60))
+        hash = sha1()
+        hash.update(password_8bit + salt.hexdigest())
+        hashed_password = salt.hexdigest() + hash.hexdigest()
 
         # make sure the hased password is an UTF-8 object at the end of the
         # process because SQLAlchemy _wants_ a unicode object for Unicode columns
         if not isinstance(hashed_password, unicode):
             hashed_password = hashed_password.decode('UTF-8')
 
-        return hashed_password
+        self._password = hashed_password
+
+    def _get_password(self):
+        """returns password
+        """
+        return self._password
+
+    password = synonym('_password', descriptor=property(_get_password,
+                                                        _set_password))
 
     def validate_password(self, password):
         """Check the password against existing credentials.
-        this method _MUST_ return a boolean.
 
-        @param password: the password that was provided by the user to
-        try and authenticate. This is the clear text version that we will
-        need to match against the (possibly) encrypted one in the database.
-        @type password: unicode object
+        :param password: the password that was provided by the user to
+            try and authenticate. This is the clear text version that we will
+            need to match against the hashed one in the database.
+        :type password: unicode object.
+        :return: Whether the password is valid.
+        :rtype: bool
+        
         """
-        return self.password == self.__encrypt_password(password)
+        hashed_pass = sha1()
+        hashed_pass.update(password + self.password[:40])
+        return self.password[40:] == hashed_pass.hexdigest()
 
 
 class Permission(DeclarativeBase):
